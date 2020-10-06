3f31bb0d1a32ff22fdc0aa5ac05cf8af45fd42b6
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-10-06 15:13:11 -0400

    Finished deliverables, included stretch goal of adding priority color and delete button. Did not include edit button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
 });
+
+// As a user, the task string that I provided should appear 
+// on the DOM after the submit button has been activated.
+
+const form = document.getElementById("create-task-form");
+form.addEventListener('submit', function(e) {
+  const text = document.getElementById("new-task-description").value
+  const textDuration = document.getElementById("duration").value
+
+  e.preventDefault()
+  const taskLi  = document.createElement("li")
+  taskLi.innerHTML = text+", Duration:"+ textDuration +'<button type="button" class="button">Edit</button>'
+  
+
+  const setColor = document.getElementById("highlight")
+    if (setColor.value === "1"){
+      taskLi.style.color = "green"
+    }
+    else if (setColor.value === "2"){
+      taskLi.style.color = "orange"
+    }
+    else if (setColor.value === "3"){
+      taskLi.style.color = "red"
+    }
+
+  const parentnode = document.querySelector("#tasks")
+  parentnode.append(taskLi) 
+})
+
+
+
+// Adding new node
+
+const deleteButton = document.getElementById("delete-button");
+deleteButton.addEventListener('click', function(e) {
+  console.log("test is working!")
+
+  // This removes ALL of the li's
+  // const allLi = document.querySelectorAll('li')
+  //for(const li of allLi){
+  //  li.remove()
+
+  // This removes the last li
+  const parentUl = document.getElementById("tasks")
+  parentUl.lastChild.remove()
+
+})
+
+const editButton = document.getElementsByClassName("button")
+editButton.addEventListener("click", function(e) {
+  console.log("test")
+})
